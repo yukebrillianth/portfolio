35e1ba0c1fde2866edd22df50f381ac8b71826e7
yuke brilliant <yukebrilliant@gmail.com>
2021-07-19 09:20:58 +0700

    rapih

--- src/components/elements/Tabs.js
+++ src/components/elements/Tabs.js
@@ -6,7 +6,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 import { Container, ContentWithPaddingXl } from "../misc/Layouts";
 import { SectionHeading } from "../misc/Headings.js";
 import { PrimaryButton as PrimaryButtonBase } from "../misc/Buttons.js";
-import { ReactComponent as StarIcon } from "../../img/star-icon.svg";
+// import { ReactComponent as StarIcon } from "../../img/star-icon.svg";
 import electrue from "../../img/portfolio/Electrue.png";
 import upvote from "../../img/portfolio/UpVote.png";
 import scrape1 from "../../img/portfolio/Scrapev1.jpg";
@@ -31,13 +31,13 @@ const CardImageContainer = styled.div`
   ${props => css`background-image: url("${props.imageSrc}");`}
   ${tw`h-56 xl:h-64 bg-center bg-cover relative rounded-t`}
 `;
-const CardRatingContainer = tw.div`leading-none absolute inline-flex bg-gray-100 bottom-0 left-0 ml-4 mb-4 rounded-full px-5 py-2 items-end`;
-const CardRating = styled.div`
-  ${tw`mr-1 text-sm font-bold flex items-end`}
-  svg {
-    ${tw`w-4 h-4 fill-current text-orange-400 mr-1`}
-  }
-`;
+// const CardRatingContainer = tw.div`leading-none absolute inline-flex bg-gray-100 bottom-0 left-0 ml-4 mb-4 rounded-full px-5 py-2 items-end`;
+// const CardRating = styled.div`
+//   ${tw`mr-1 text-sm font-bold flex items-end`}
+//   svg {
+//     ${tw`w-4 h-4 fill-current text-orange-400 mr-1`}
+//   }
+// `;
 
 const CardHoverOverlay = styled(motion.div)`
   background-color: rgba(255, 255, 255, 0.5);
@@ -45,7 +45,7 @@ const CardHoverOverlay = styled(motion.div)`
 `;
 const CardButton = tw(PrimaryButtonBase)`text-sm`;
 
-const CardReview = tw.div`font-medium text-xs text-gray-600`;
+// const CardReview = tw.div`font-medium text-xs text-gray-600`;
 
 const CardText = tw.div`p-4 text-gray-900`;
 const CardTitle = tw.h5`text-lg font-semibold group-hover:text-blue-600`;
