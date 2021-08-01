8381a53823d8abf1b99f80a6a5ea587176a798a3
yuke brilliant <yukebrilliant@gmail.com>
2021-08-01 20:48:31 +0700

    add whatsapp button

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,4 +1,22 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import waIcon from '../../../img/wa-icon.svg';
 import { About, Contact, Footer, Hero, Navbar, Tabs } from '../../elements'
+
+const CtaContainer = styled.a`
+    position:fixed;
+    width:60px;
+    height:60px;
+    bottom:40px;
+    right:40px;
+    z-index:100;
+
+    @media (max-width: 768px) { 
+        right:20px;
+        bottom:20px;
+     }
+`
+const CtaImage = styled.img``
 
 const Home = () => {
     return (
@@ -9,6 +27,9 @@ const Home = () => {
             <Tabs color="blue" />
             <Contact />
             <Footer />
+            <CtaContainer href="https://wa.me/send?phone=6285755773985&text=Halo!" target="_blank">
+                <CtaImage src={waIcon} />
+            </CtaContainer>
         </section>
     );
 }
