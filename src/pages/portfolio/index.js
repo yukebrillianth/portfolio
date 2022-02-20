45a328dc3ddc1e63a6a606886e05b74c7af23eca
yuke brilliant <yukebrilliant@gmail.com>
2022-02-20 18:08:38 +0700

    Feat: Add portfolio page

---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,10 @@
+import { Navbar } from "../../components";
+
+export default function Portfolio() {
+    return (
+        <>
+            <Navbar />
+            <h1>Portfolio</h1>
+        </>
+    );
+}
