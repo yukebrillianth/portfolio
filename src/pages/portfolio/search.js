d989c7ab651da82051dd37c1718501543df894c1
yuke brilliant <yukebrilliant@gmail.com>
2022-02-22 00:15:55 +0700

    Feat: Add portfolio search page base

---
+++ src/pages/portfolio/search.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Navbar } from "../../components";
+
+export default function SearchPortfolio() {
+    const router = useRouter();
+    const { q } = router.query;
+    return (
+        <>
+            <Navbar />
+            <Link href={{ pathname: '/portfolio/search', query: { q: 'this way' } }}><a>path</a></Link>
+            <h1 className="text-dark mt-4">search</h1>
+            <h1 className="text-dark mt-4">{q}</h1>
+        </>
+    );
+}
