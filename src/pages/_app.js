ceeb3116c3d00239eb2e8c4f916c4f407122f5d9
yukebrillianth <yukebrilliant@gmail.com>
2022-09-25 13:47:28 +0700

    Feat: add browser theme color

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
         {/* <link href="/assets/fonts/stylesheet.css" rel="stylesheet" /> */}
       </Head>
       <Component {...pageProps} />
