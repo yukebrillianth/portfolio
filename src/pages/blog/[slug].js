76626c6db223fb08f0f914f6e896af525e113010
yuke brilliant <yukebrilliant@gmail.com>
2023-03-27 17:03:38 +0700

    Feat: Blog post

---
+++ src/pages/blog/[slug].js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+import { Navbar } from "../../components";
+
+export default function PortfolioDetail() {
+    const router = useRouter();
+    const { slug } = router.query;
+    return (
+        <>
+            <Navbar />
+            <h1 className="text-dark mt-4">Judul Post: {slug}</h1>
+        </>
+    );
+}
