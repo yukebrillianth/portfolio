fdb354c8439affbe116f3bd6f3b73b8088f7e964
yuke brilliant <yukebrilliant@gmail.com>
2023-12-03 17:43:53 +0700

    Fix: remove wm

--- src/pages/sign-in/[[...index]].js
+++ src/pages/sign-in/[[...index]].js
@@ -1,20 +1,46 @@
 import { SignIn } from "@clerk/nextjs";
 import tw from "twin.macro";
+import Script from 'next/script'
 
 const SignInPage = () => (
-    <main className="flex w-full min-h-screen justify-center items-center bg-dark py-8" css={[tw`
+    <>
+        <main className="flex w-full min-h-screen justify-center items-center bg-dark py-8" css={[tw`
     bg-dark
     background-size[400%]
     md:background-size[300%]
     lg:background-size[200%]
     xl:background-size[cover]
   `,
-        `
+            `
     background-image: url(/assets/backgrounds/grid-dark.svg);
     background-position: 100%;
   `]}>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-    </main>
+            <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        </main>
+        <Script>
+            {`
+            // Create a new observer instance
+            let observer = new MutationObserver((mutations) => {
+              // Look through all mutations that just occured
+              for(let mutation of mutations) {
+                // If the addedNodes property has one or more nodes
+                if(mutation.addedNodes.length) {
+                  let elements = document.querySelectorAll("div.cl-card > div"");
+                  if(elements.length > 4) {
+                    elements[4].classList.add("hidden");
+                    // Once the class is added, we don't need to observe anymore
+                    observer.disconnect();
+                    break;
+                  }
+                }
+              }
+            });
+            
+            // Start observing the document with the configured parameters
+            observer.observe(document.body, { childList: true, subtree: true });            
+        `}
+        </Script>
+    </>
 );
 
 export default SignInPage;
--- src/pages/sign-up/[[...index]].js
+++ src/pages/sign-up/[[...index]].js
@@ -1,19 +1,45 @@
 import { SignUp } from "@clerk/nextjs";
 import tw from "twin.macro";
+import Script from 'next/script'
 
 const SignUpPage = () => (
-    <main className="flex w-full min-h-screen justify-center items-center bg-dark py-8" css={[tw`
+    <>
+        <main className="flex w-full min-h-screen justify-center items-center bg-dark py-8" css={[tw`
     bg-dark
     background-size[400%]
     md:background-size[300%]
     lg:background-size[200%]
     xl:background-size[cover]
   `,
-        `
+            `
     background-image: url(/assets/backgrounds/grid-dark.svg);
     background-position: 100%;
   `]}>
-        <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-    </main>
+            <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+        </main>
+        <Script>
+            {`
+            // Create a new observer instance
+            let observer = new MutationObserver((mutations) => {
+              // Look through all mutations that just occured
+              for(let mutation of mutations) {
+                // If the addedNodes property has one or more nodes
+                if(mutation.addedNodes.length) {
+                  let elements = document.querySelectorAll("div.cl-card > div");
+                  if(elements.length > 4) {
+                    elements[4].classList.add("hidden");
+                    // Once the class is added, we don't need to observe anymore
+                    observer.disconnect();
+                    break;
+                  }
+                }
+              }
+            });
+            
+            // Start observing the document with the configured parameters
+            observer.observe(document.body, { childList: true, subtree: true });            
+        `}
+        </Script>
+    </>
 );
 export default SignUpPage;
