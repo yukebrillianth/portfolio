75f914813d7a79ec92585c7754206f2588e57c05
yukebrillianth <yukebrilliant@gmail.com>
2024-03-17 03:02:22 +0700

    fix: revert blog path

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 module.exports = {
-    images: {
-        domains: ['media.graphassets.com']
-    },
-    async rewrites() {
-    return [
-      {
-        source: "/blog",
-        destination: "https://hashnode-starter-kit-henna.vercel.app/blog",
-      },
-      {
-        source: "/blog/:path*",
-        destination: "https://hashnode-starter-kit-henna.vercel.app/blog/:path*",
-      },
-    ];
+  images: {
+    domains: ["media.graphassets.com"],
   },
-}
+};
