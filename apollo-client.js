488fdda2fa4c60d717af843f3ce732f01e6fb3a8
yukebrillianth <yukebrilliant@gmail.com>
2024-03-17 03:03:48 +0700

    Feat: add blog api endpoint

--- apollo-client.js
+++ apollo-client.js
@@ -1,29 +1,40 @@
 import {
-    ApolloClient,
-    InMemoryCache,
-    HttpLink,
-    from,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  from,
+  ApolloLink,
 } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 
 const errorLink = onError(({ graphqlErrors, networkError }) => {
-    if (graphqlErrors) {
-        graphqlErrors.map(({ message }) => {
-            alert(`Graphql error ${message}`);
-        });
-    }
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
 });
 
-const link = from([
-    errorLink,
-    new HttpLink({
-        uri: process.env.NEXT_PUBLIC_CONTENT_API_ENDPOINT,
-    }),
+const link1 = from([
+  errorLink,
+  new HttpLink({
+    uri: process.env.NEXT_PUBLIC_CONTENT_API_ENDPOINT,
+  }),
+]);
+const link2 = from([
+  errorLink,
+  new HttpLink({
+    uri: process.env.NEXT_PUBLIC_BLOG_API_ENDPOINT,
+  }),
 ]);
 
 const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link
+  cache: new InMemoryCache(),
+  link: ApolloLink.split(
+    (operation) => operation.getContext().clientName == "blog",
+    link2,
+    link1
+  ),
 });
 
 export default client;
