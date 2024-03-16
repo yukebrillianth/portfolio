ff1ada5687dd2b6c618b43fad8d85c06cbee4b32
Yuke Brilliant H <61053094+yukebrillianth@users.noreply.github.com>
2024-03-17 02:21:50 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@ module.exports = {
     images: {
         domains: ['media.graphassets.com']
     },
+    async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "hhttps://hashnode-starter-kit-henna.vercel.app/blog", -> Replace https://starter-kit-rose-seven.vercel.app with your own Vercel deployment URL from step 1
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://hashnode-starter-kit-henna.vercel.app/blog/:path*", -> Replace https://starter-kit-rose-seven.vercel.app with your own Vercel deployment URL from step 1
+      },
+    ];
+  },
 }
